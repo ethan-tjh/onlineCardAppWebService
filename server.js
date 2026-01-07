b8200b4ba8798c695434784ea48f9b50caceaa07
ethan-tjh <ethantan905@gmail.com>
2026-01-07 12:25:24 +0800

    Added POST route for /allcards

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.listen(port, () => {
     console.log('Server running on port', port);
 });
+// GET
 app.get('/allcards', async (req, res) => {
     try {
         let connection = await mysql.createConnection(dbConfig);
@@ -25,5 +26,17 @@ app.get('/allcards', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({message: 'Server error for allcards'});
+    }
+});
+// POST
+app.post('/allcards', async (req, res) => {
+    const {card_name, card_pic} = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?, ?)', [card_name, card_pic]);
+        res.status(201).json({message: 'Card ' + card_name + ' added successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not add card ' + card_name});
     }
 });
